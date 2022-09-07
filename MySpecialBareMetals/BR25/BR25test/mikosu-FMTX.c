e9c9c4eb428ab819f64c924b7960bc12f0903e77
Andrey Grigoryev <andrey-grigorev-2006@yandex.ru>
2022-09-08 01:22:42 +0300

    FMTX attempt 2 (kinda works)
    There is a strong carrier at 96 MHz,
    however the chip reboots after some time
    and even though i put the data into the tx buffer,
    the carrier is not modulated at all?

--- MySpecialBareMetals/BR25/BR25test/mikosu-FMTX.c
+++ MySpecialBareMetals/BR25/BR25test/mikosu-FMTX.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include <xprintf.h>
 #include <jl_regs.h>
+#include <jl_irq.h>
+#include <wallclk.h>
 
 
 void uputc(int c) {
 	reg32_write(UART0_base+UARTx_BUF, c);
-	while (!reg32_rsmask(UART0_base+UARTx_CON0_TPND));
-	reg32_wsmask(UART0_base+UARTx_CON0_CLRTPND, 1);
+	while (!reg32_rsmask(UART0_base+UARTx_CON0_tpnd));
+	reg32_wsmask(UART0_base+UARTx_CON0_clrtpnd, 1);
 }
 
 
@@ -37,23 +39,35 @@ void hexdump(void *ptr, int len) {
 }
 
 
-void delay(uint32_t ms) {
-	while (ms--) // TODO!
-		for (volatile int i = 1000; i; i--);
+
+
+
+volatile uint32_t fmtx_buf[2][1024];
+volatile uint64_t lasttime;
+
+void IRQ_HANDLER fmtx_isr(void) {
+	reg32_wsmask(FM_base+FM_TX_CON1, 6, 1, 1); // clear pending
+	int slice = reg32_rsmask(FM_base+FM_TX_CON0, 4, 1);
+
+	for (int i = 0; i < 1024; i++)
+		fmtx_buf[slice][i] = reg32_read(RAND_base+RAND_R64L);
+
+	//xprintf("%08x\n", fmtx_buf[slice][0]);
+
+	uint32_t ctime = micros() - lasttime;
+	lasttime = micros();
+
+	xprintf("!!! FMTX IRQ, slice=%d, period=%u\n", slice, 1024 * 1000000 / ctime);
 }
 
-
-
-uint32_t fmtx_buf[2][128];
-
 void fmtx_on(void) {
-	reg32_wsmask(FM_base+FM_TX_CON0, 7, 1, 1);
 	reg32_wsmask(FM_base+FM_TX_CON0, 0, 1, 1);
+	reg32_wsmask(FM_base+FM_TX_CON0, 7, 1, 1);
 }
 
 void fmtx_off(void) {
-	reg32_wsmask(FM_base+FM_TX_CON0, 7, 1, 0);
 	reg32_wsmask(FM_base+FM_TX_CON0, 0, 1, 0);
+	reg32_wsmask(FM_base+FM_TX_CON0, 7, 1, 0);
 }
 
 void fmtx_init(void) {
@@ -75,7 +89,7 @@ void fmtx_init(void) {
 	reg32_write(FM_base+FM_TX_LEN,      0);
 
 	for (int i = 160; i >= 0; i--) {
-		reg32_write(FM_base+FM_TX_ADR, i);
+		reg32_write(FM_base+FM_TX_ADR, i << 2);
 		delay(5);
 		reg32_wsmask(FM_base+FM_TX_CON0, 5, 1, 1);
 		delay(5);
@@ -167,7 +181,7 @@ void fmtx_init(void) {
 	reg32_wsmask(FM_base+FM_TX_CON0, 0, 1, 1);
 	delay(10);
 
-	reg32_wsmask(FM_base+FM_TX_CON1, 7, 1, 1);
+	reg32_wsmask(FM_base+FM_TX_CON1, 6, 1, 1);
 	delay(10);
 
 	reg32_wsmask(FM_base+FM_TX_CON0, 1, 1, 1); // <--- stereo flag
@@ -178,39 +192,34 @@ void fmtx_init(void) {
 
 	reg32_write(FM_base+FM_TX_FREQ, 0x15400000);
 
-	reg32_write(FM_base+FM_TX_LEN, 128);
+	reg32_write(FM_base+FM_TX_LEN, 1024);
 	delay(500);
 
-	reg32_wsmask(FM_base+FM_TX_CON0, 7, 1, 1);
+	reg32_wsmask(FM_base+FM_TX_CON0, 3, 1, 1);
 }
 
+#if 0
 void fmtx_set_freq(int freq) {
-
-	// super calcuations
-
-	/*reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 31, 1, 0);
-
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON,  11, 1, 1);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 31, 1, 0);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 11, 1, 1);
 	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 22, 0x3, 0x1);
 	reg32_wsmask(ANA_base+ANA_WLA_CON19, 21, 1, 1);
-
-	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON2,  2, 0xf, 0x8);
-
-	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON2,  0, 0x3, 0x3);
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON,  24, 0x7, 0x1);
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON,  27, 0x7, 0x3);
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON,  10, 1, 1);
+	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON2, 2, 0xf, 0x8); //<--- divider[%12 - 1]
+	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON2, 0, 0x3, 0x3);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 24, 0x7, 0x1);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 27, 0x7, 0x3);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 10, 1, 1);
 	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 19, 1, 1);
-
 	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 31, 1, 1);
 
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 0, 1, 1);
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 0, 1, 1); // enable
 	delay(150);
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 1, 1, 1);*/
+	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON, 1, 1, 1); // release reset
 
-	reg32_write(FM_base+FM_TX_FREQ, (0x15 << 24) | 0x400000); // %18, %26
+	reg32_write(FM_base+FM_TX_FREQ, (0x15<<24)|0x400000); // (%18 << 24) | %26
 	delay(30);
 }
+#endif
 
 void fmtx_analog_init(void) {
 	reg32_wsmask(ANA_base+ANA_WLA_CON18,  1, 1, 1);
@@ -218,6 +227,8 @@ void fmtx_analog_init(void) {
 }
 
 void fm_emitter_init(void) {
+	irq_attach(47, fmtx_isr);
+
 	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON1, 24, 0x3, 0x1); // clk sel
 	reg32_wsmask(FM_base+FM_TX_CON1, 3, 1, 1);
 	fmtx_init();
@@ -226,6 +237,60 @@ void fm_emitter_init(void) {
 	reg32_wsmask(CLOCK_base+CLOCK_CLK_CON1, 12, 0x3, 0x1); // clk sel
 	reg32_wsmask(PORTB_base+PORTx_DIRn(1), 0); // PB1 = out
 }
+
+
+
+struct JieLi_ExceptFrame {
+	// general purpose
+	uint32_t gpr[16];
+
+	// special function
+	union {
+		uint32_t sfr[16];
+		struct {
+			uint32_t reti, rete, retx, rets;
+			uint32_t sr4,  psr,  cnum, sr7;
+			uint32_t sr8,  sr9,  sr10, icfg;
+			uint32_t usp,  ssp,  sp,   pc;
+		};
+	};
+};
+
+void ExceptionHandler(struct JieLi_ExceptFrame *ef) {
+	xputs("\e[1;33;44m=========== JieLi Crashed! ===========\e[0m\n");
+
+	uint32_t *p;
+
+	xputs("\n---- General purpose regs ----\n");
+	p = ef->gpr;
+	xprintf(" r0: <%08x>   r1: <%08x>   r2: <%08x>   r3: <%08x>\n", p[ 0], p[ 1], p[ 2], p[ 3]);
+	xprintf(" r4: <%08x>   r5: <%08x>   r6: <%08x>   r7: <%08x>\n", p[ 4], p[ 5], p[ 6], p[ 7]);
+	xprintf(" r8: <%08x>   r9: <%08x>  r10: <%08x>  r11: <%08x>\n", p[ 8], p[ 9], p[10], p[11]);
+	xprintf("r12: <%08x>  r13: <%08x>  r14: <%08x>  r15: <%08x>\n", p[12], p[13], p[14], p[15]);
+
+	xputs("\n---- Special function regs ----\n");
+	p = ef->sfr;
+	xprintf("reti: <%08x>  rete: <%08x>  retx: <%08x>  rets: <%08x>\n", p[ 0], p[ 1], p[ 2], p[ 3]);
+	xprintf(" sr4: <%08x>   psr: <%08x>  cnum: <%08x>   sr7: <%08x>\n", p[ 4], p[ 5], p[ 6], p[ 7]);
+	xprintf(" sr8: <%08x>   sr9: <%08x>  sr10: <%08x>  icfg: <%08x>\n", p[ 8], p[ 9], p[10], p[11]);
+	xprintf(" usp: <%08x>   ssp: <%08x>    sp: <%08x>    pc: <%08x>\n", p[12], p[13], p[14], p[15]);
+}
+
+__attribute__((naked)) void ExceptionHandler_entry(void) {
+	// store all the regs - make an exception frame
+	asm ("[--sp] = {pc, sp, ssp, usp, icfg, sr10, sr9, sr8, sr7, cnum, psr, sr4, rets, retx, rete, reti}");
+	asm ("[--sp] = {r15-r0}");
+
+	// pass the pointer to the exception frame
+	asm ("r0 = sp");
+
+	// call the handler
+	asm ("call ExceptionHandler");
+
+	// halt
+	asm ("1: idle\ngoto 1b");
+}
+
 
 
 
@@ -245,74 +310,14 @@ void JieLi(uint32_t r0, uint32_t r1, uint32_t r2, uint32_t r3) {
 
 	/*==================================================================*/
 
-	for (int i = 0; i < 128; i++)
-		fmtx_buf[0][i] = reg32_read(RAND_base+RAND_R64L);
+	irq_attach(1, ExceptionHandler_entry);
+
+	wallclk_init();
+
+	xputs("Start\n");
 
 	fm_emitter_init();
-	fmtx_set_freq(9000); // 90 mhz sorta like that
+	//fmtx_set_freq(9000); // 90 mhz sorta like that
 
-	/*==================================================================*/
-
-	#if 0
-	///----------- dac init
-	reg32_wsmask(CLOCK_base+CLOCK_PLL_CON1, 30, 1, 1);
-
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 19, 0x3, 0x3);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 18, 1, 1);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 17, 1, 1);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 16, 1, 1);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 15, 1, 0);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 14, 1, 1);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 8, 1, 0);
-	reg32_wsmask(ANA_base+ANA_DAA_CON0, 6, 1, 0);
-
-	///----------- dac vdd power on
-	//reg32_wsmask(ANA_base+ANA_DAA_CON2, 11, 0x7, 0x0);
-
-	//reg32_wsmask(ANA_base+ANA_DAA_CON0, 25, 0x7, 0x0); // vdd voltage
-
-	//reg32_wsmask(ANA_base+ANA_DAA_CON0, 22, 1, 1);
-	//reg32_wsmask(ANA_base+ANA_DAA_CON0, 24, 1, 1);
-	#endif
-
-	/*==================================================================*/
-
-	#if 0
-	///----------- open linein
-	reg32_wsmask(PORTA_base+PORTx_DIRn(3), 1); // PA3 in
-	reg32_wsmask(PORTA_base+PORTx_DIEn(3), 0); // PA3 digital in disable
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux0l_en, 1); // AMUX0L enable
-
-	reg32_wsmask(PORTA_base+PORTx_DIRn(4), 1); // PA4 in
-	reg32_wsmask(PORTA_base+PORTx_DIEn(4), 0); // PA4 digital in disable
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux0r_en, 1); // AMUX0R enable
-
-	reg32_wsmask(PORTB_base+PORTx_DIRn(6), 1); // PB6 in
-	reg32_wsmask(PORTB_base+PORTx_DIEn(6), 0); // PB6 digital in disable
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux1l_en, 1); // AMUX1L enable
-
-	reg32_wsmask(PORTB_base+PORTx_DIRn(7), 1); // PB7 in
-	reg32_wsmask(PORTB_base+PORTx_DIEn(7), 0); // PB7 digital in disable
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux1r_en, 1); // AMUX1R enable
-
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux_en, 1); // AMUX enable
-
-	///----------- gain
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux_gain, 0); // 2x gain
-
-	///----------- mute
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux_mute, 0); // mute
-
-	///----------- combine
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux_lrmix, 0x0);
-
-	///----------- linein bias
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux0l_biasen, 0); // AMUX0L bias
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux0r_biasen, 0); // AMUX0R bias
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux1l_biasen, 0); // AMUX1L bias
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux1r_biasen, 0); // AMUX1R bias
-
-	///----------- amux bias
-	reg32_wsmask(ANA_base+ANA_DAA_CON1_amux_biasen, 0); // amux bias
-	#endif
+	xputs("End\n");
 }
